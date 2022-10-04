ed2d40005ca77fcc1ebf764b996713867606e4c1
khizarsiddiqui <khizarsiddiqui10@gmail.com>
2022-10-05 00:13:52 +0500

    first commit

---
+++ ps.py
@@ -0,0 +1,8 @@
+# first commit
+
+# Here are some of the concepts for particle system:
+# • Developing a mathematical model for a fountain particle system
+# • Using GPU shaders for computation
+# • Using textures and billboarding to imitate complex 3D objects
+# • Using OpenGL rendering features such as blending, depth masks, and alpha channels to draw semitransparent objects
+# • Using a camera model to draw 3D perspective views
